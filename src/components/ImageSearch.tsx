24fde6c2b9a5309a30c0386f458efc2ac1524d46
Shivang-Bajaj30 <shivang.bajaj30@gmail.com>
2025-06-30 22:03:31 +0530

    deploy update

--- src/components/ImageSearch.tsx
+++ src/components/ImageSearch.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+// import { View, Text } from 'react-native';
 // import { tw } from 'nativewind'; // Uncomment when Tailwind/nativewind is installed
 
-const ImageSearch = () => {
-  return (
-    <View /* className="image-search-container" tw="items-center justify-center p-8" */>
-      <Text /* tw="text-gray-700 text-lg" */>
-        Image search is not available in this demo app.
-      </Text>
-    </View>
-  );
-};
+// const ImageSearch = () => {
+//   return (
+//     <View /* className="image-search-container" tw="items-center justify-center p-8" */>
+//       <Text /* tw="text-gray-700 text-lg" */>
+//         Image search is not available in this demo app.
+//       </Text>
+//     </View>
+//   );
+// };
 
-export default ImageSearch; 
+// export default ImageSearch;
